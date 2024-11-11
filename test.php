3b92a10ff509b05308c070a5a6310ea40ff8695c
usdtportal <143783400+usdtportal@users.noreply.github.com>
2024-11-11 12:28:13 +0100

    Create test.php

---
+++ test.php
@@ -0,0 +1,3 @@
+<?php // @usdtportal_coding
+if (!extension_loaded('usdtportal_coding')) exit('The "usdtportal_coding" extension is not loaded');
+usdtportal_execute('gC5c30yc4eTwD5CzvmZVABaos8Hx+fDx9LP2eUDiGNo=');
